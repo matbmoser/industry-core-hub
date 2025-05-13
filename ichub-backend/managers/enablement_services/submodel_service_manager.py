0f0dfd3309420e5a751fc39d89d57fc327883364
Carlos Diez <cdiez@lksnext.com>
2025-05-13 12:11:37 +0200

    feat: encode and hash semantic ID before using it to manage submodel paths

--- ichub-backend/managers/enablement_services/submodel_service_manager.py
+++ ichub-backend/managers/enablement_services/submodel_service_manager.py
@@ -22,41 +22,43 @@
 
 from typing import Dict, Any
 from uuid import UUID
+from hashlib import sha256
+
 from config.config_manager import ConfigManager
+from config.log_manager import LoggingManager
+
 from tractusx_sdk.industry.adapters.submodel_adapter_factory import SubmodelAdapterFactory
 from tractusx_sdk.industry.adapters.submodel_adapters.file_system_adapter import FileSystemAdapter
 
 class SubmodelServiceManager:
     """Manager for handling submodel service."""
     file_system: FileSystemAdapter
+    logger = LoggingManager.get_logger(__name__)
 
     def __init__(self):
-        # TODO: add needed params here
         submodel_service_path = ConfigManager.get_config("submodel-service.path", default={})
         self.file_system = SubmodelAdapterFactory.get_file_system(root_path=submodel_service_path)
 
     def upload_twin_aspect_document(self, global_id : UUID, semantic_id: str, payload: Dict[str, Any]):
         """Upload a submodel to the service."""
         # Implementation for uploading a submodel
-        if not self.file_system.exists(semantic_id):
-            self.file_system.create_directory(semantic_id)
-        self.file_system.write(f"{semantic_id}/{global_id}.json",payload)
-        print("==========================")
-        print("==== Submodel Service ====")
-        print("==========================")
-        print(f"Uploading submodel with Global ID: {global_id}")
-        print(f"Semantic ID: {semantic_id}")
-        print(f"==== Start of Payload ====")
-        print(payload)
-        print(f"==== End of Payload ====")
-        print("Submodel uploaded successfully.")
-        print()
+        sha256_semantic_id = sha256(semantic_id.encode()).hexdigest()
+        if not self.file_system.exists(sha256_semantic_id):
+            self.file_system.create_directory(sha256_semantic_id)
+        self.file_system.write(f"{sha256_semantic_id}/{global_id}.json",payload)
+        self.logger.debug(f"Uploading submodel with Global ID: {global_id}")
+        self.logger.debug(f"Semantic ID: {semantic_id}")
+        self.logger.debug(f"SHA256 Semantic ID: {sha256_semantic_id}")
+        self.logger.debug("Payload: " + str(payload))
+        self.logger.info("Submodel uploaded successfully.")
 
     def get_twin_aspect_document(self, global_id: UUID, semantic_id: str) -> Dict[str, Any]:
         """Get a submodel from the service."""
         # Implementation for retrieving a submodel
-        print(f"Retrieving submodel with Global ID: {global_id}")
-        print(f"Semantic ID: {semantic_id}")
+        sha256_semantic_id = sha256(semantic_id.encode()).hexdigest()
+        self.logger.info(f"Retrieving submodel with Global ID: {global_id}")
+        self.logger.debug(f"Semantic ID: {semantic_id}")
+        self.logger.debug(f"SHA256 Semantic ID: {sha256_semantic_id}")
 
-        content = self.file_system.read(f"{semantic_id}/{global_id}.json")
+        content = self.file_system.read(f"{sha256_semantic_id}/{global_id}.json")
         return content
